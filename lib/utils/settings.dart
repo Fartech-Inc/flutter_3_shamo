3f5a1baa0d5e60c167a5477ae4bcb4cc1dbb1912
reonaldi152 <reonaldi@team.superfk.co>
2025-02-05 09:30:09 +0700

    change token vc

--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,3 +1,3 @@
 const appId = '5c90d4bfee4b427aa4c75c82bc1c9b1b';
 const token =
-    '007eJxTYPhzOnpC+dp1dl2rvOJrLjwT04pjiulOdGrOrZfwiI5J1VJgME22NEgxSUpLTTVJMjEyT0w0STY3TbYwSko2TLZMMkxacHVBekMgI0PQNSVGRgYIBPFZGbIzC3IyGRgAPRkfVQ==';
+    '007eJxTYEiYX5jUzXl+isPuveviU2/XrnltcTsl7aO08fkZO3t+3vutwGCabGmQYpKUlppqkmRiZJ6YaJJsbppsYZSUbJhsmWSYxHB2UXpDICODyo2/jIwMEAjiszJkZxbkZDIwAABstSPK';
